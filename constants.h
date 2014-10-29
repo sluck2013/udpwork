c15cc9f8b735ffce38463d3f056e7e6b1c10fadc
sluck2013 <yswang2013@gmail.com>
2014-10-29 15:14:16 -0400

    delete header.h

--- constants.h
+++ constants.h
@@ -1,6 +1,10 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define MYCONST_1 (5)
+/******** ERRORS *******/
+#define ERR_OPEN_CLIENT_IN "Cannot open client.in. Make sure it is in the current directory."
+#define ERR_READ_CLIENT_IN "Reading client.in"
 
+/******** OTHER CONSTS ********/
+#define MAXCHAR (256)
 #endif
